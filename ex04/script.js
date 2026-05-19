5fbdc73d6f6ce002819c4afd7d9f7d34135523d3
FelipeAndrade-R <felipeka07@gmail.com>
2026-05-19 13:56:12 -0300

    Update script.js

--- ex04/script.js
+++ ex04/script.js
@@ -1,12 +1,10 @@
 const listaNumeros = [];
 const listaSemDuplicados = [];
 
-// Ler 10 números
 for (let i = 0; i < 10; i++) {
     listaNumeros.push(Number(prompt(`Digite o ${i + 1}º número:`)));
 }
 
-// Remover duplicados
 for (let i = 0; i < listaNumeros.length; i++) {
 
     let numero = listaNumeros[i];
@@ -16,7 +14,6 @@ for (let i = 0; i < listaNumeros.length; i++) {
     }
 }
 
-// Mostrar resultado
 alert(
     "Vetor Original: " + listaNumeros.join(", ") +
     "\nVetor Sem Duplicatas: " + listaSemDuplicados.join(", ")
